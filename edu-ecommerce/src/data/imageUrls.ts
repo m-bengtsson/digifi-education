02d51c8d2e6c455612c91df4ec5083d68b229419
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2026-01-29 12:05:40 +0100

    Remove unused code

---
+++ edu-ecommerce/src/data/imageUrls.ts
@@ -0,0 +1,6 @@
+const imageUrls = {
+  banner:
+    "https://nycdn.nyehandel.se/store_91f5fbde-a76f-4822-9565-1c254755b651/images/isjwqkjCrnFYNf01jASQTavhRCnUaW0UAr8VSlke.webp",
+};
+
+export default imageUrls;
